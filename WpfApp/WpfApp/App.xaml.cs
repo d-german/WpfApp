605eed505e2fe9aec6c18d861f2d347a90340e31
Damon German <damon.german@hyland.com>
2021-05-28 19:51:26 -0500

    made some changes

--- WpfApp/WpfApp/App.xaml.cs
+++ WpfApp/WpfApp/App.xaml.cs
@@ -1,32 +1,16 @@
 ﻿using System.Windows;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace WpfApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
-        private readonly ServiceProvider _serviceProvider;
-
-        public App()
-        {
-            var services = new ServiceCollection();
-            ConfigureServices(services);
-            _serviceProvider = services.BuildServiceProvider();
-        }
-
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<IWeatherService, WeatherService>();
-            services.AddSingleton<MainWindow>();
-        }
-
         private void OnStartup(object sender, StartupEventArgs e)
         {
-            var mainWindow = _serviceProvider.GetService<MainWindow>();
-            mainWindow?.Show();
+            var mainWindow = new MainWindow(new WeatherService());
+            mainWindow.Show();
         }
     }
 }
